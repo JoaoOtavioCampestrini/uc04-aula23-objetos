1d33f8ab5c23efe763e046f4d462f0686f43d064
Joao Otavio <joaootaviocampestrini@gmail.com>
2025-01-23 20:30:37 -0300

    primeiro commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const tupla = ("cavalo", "manga larga", "marrom", "1.90", "M",)
+
+const cavalos = [
+["cavalo" , "manga larga", "marrom", "1.90", "M"],
+["cavalo" , "manga larga", "marrom", "1.90", "M"],
+["cavalo" , "manga larga", "marrom", "1.90", "M"],
+["cavalo" , "manga larga", "marrom", "1.90", "M"],
+]
