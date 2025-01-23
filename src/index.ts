32bbc0ff2982649be8a7112f3ef486266ea6b21e
Joao Otavio <joaootaviocampestrini@gmail.com>
2025-01-23 20:31:54 -0300

    segundo commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
-const tupla = ("cavalo", "manga larga", "marrom", "1.90", "M",)
+import Animal from "./animal";
 
-const cavalos = [
-["cavalo" , "manga larga", "marrom", "1.90", "M"],
-["cavalo" , "manga larga", "marrom", "1.90", "M"],
-["cavalo" , "manga larga", "marrom", "1.90", "M"],
-["cavalo" , "manga larga", "marrom", "1.90", "M"],
-]
+const cavalos: Animal[] = [
+  new Animal("cavalo", "manga larga", "marrom", 1.9, "M"),
+  new Animal("cavalo", "crioulo", "marrom", 1.9, "M"),
+  new Animal("cavalo", "marchador", "marrom", 1.9, "M"),
+];
+
+for (const cavalo of cavalos) {
+  const { especie, raca, cor, tamanho, sexo } = cavalo;
+  const racaCor = cavalo.getRacaCor();
+  cavalo.nome = "";
+
+  console.log(`
+    Espécie: ${especie}
+    Raça: ${raca}
+    Cor: ${cor}
+    Tamanho: ${tamanho}
+    Sexo: ${sexo}
+    Raça e Cor ${racaCor}
+    `);
+}
